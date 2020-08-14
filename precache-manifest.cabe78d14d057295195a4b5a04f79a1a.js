a984ece40e807e035bc40bfdcc2aab8e8153a526
asgaraliyev <asgaraliyev2001@gmail.com>
2020-08-14 16:13:11 +0400

    Updates

---
+++ precache-manifest.cabe78d14d057295195a4b5a04f79a1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508f78e96099eea0b6b4541695c4426e",
+    "url": "/asgaraliyev-react-app/index.html"
+  },
+  {
+    "revision": "1d4f15954362177dd93b",
+    "url": "/asgaraliyev-react-app/static/css/main.d2ce5a7b.chunk.css"
+  },
+  {
+    "revision": "abc542240fd7f8951e3f",
+    "url": "/asgaraliyev-react-app/static/js/2.c2a87ab8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/asgaraliyev-react-app/static/js/2.c2a87ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d4f15954362177dd93b",
+    "url": "/asgaraliyev-react-app/static/js/main.884d05be.chunk.js"
+  },
+  {
+    "revision": "6b23f1ab51f7c8d89c80",
+    "url": "/asgaraliyev-react-app/static/js/runtime-main.848a75d6.js"
+  }
+]);
